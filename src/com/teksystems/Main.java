792b4c5edd95985abba7a2308709996f76e5e329
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2022-02-18 14:46:15 -0600

    First Commit

---
+++ src/com/teksystems/Main.java
@@ -0,0 +1,15 @@
+package com.teksystems;
+
+public class Main {
+    public static void main(String[] args) {
+        int seconds = 86399;
+        int p1 = seconds % 60;
+        int p2 = seconds / 60;
+        int p3 = p2 % 60;
+        p2/=60;
+        System.out.printf(p2 + ":" + p3 + ":" + p1);
+        // System.out.print("\n");
+    }
+}
+
+
